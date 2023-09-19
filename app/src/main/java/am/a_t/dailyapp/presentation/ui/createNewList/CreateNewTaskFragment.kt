e798bbebf2e7138c55bc8921ff1687b62a9e6c28
ARSEN TAROYAN <arsen2003taroyan@gmail.com>
2023-09-19 21:15:28 +0400

    Navigation

---
+++ app/src/main/java/am/a_t/dailyapp/presentation/ui/createNewList/CreateNewTaskFragment.kt
@@ -0,0 +1,25 @@
+package am.a_t.dailyapp.presentation.ui.createNewList
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import am.a_t.dailyapp.R
+import am.a_t.dailyapp.databinding.FragmentCreateNewTaskBinding
+import androidx.fragment.app.viewModels
+
+class CreateNewTaskFragment : Fragment() {
+    private lateinit var binding: FragmentCreateNewTaskBinding
+    private val viewModel: CreateNewTaskViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCreateNewTaskBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+}
